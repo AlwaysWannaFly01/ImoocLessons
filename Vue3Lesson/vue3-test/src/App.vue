8c733d977ba0863b518ebb6a46d1957d92dd1a54
alwayswannafly <15007159069@163.com>
2020-12-09 11:07:33 +0800

    vue3中的生命周期钩子函数

--- Vue3Lesson/vue3-test/src/App.vue
+++ Vue3Lesson/vue3-test/src/App.vue
@@ -1,27 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+        <button v-for="(item,index) in people" :key="index" @click="selectFun(index)">
+            {{ item }}:{{ index }}
+        </button>
+        <p>选择了[{{ selectedPeron }}]</p>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {
+    reactive,
+    toRefs,
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onRenderTracked,
+    onRenderTriggered
+} from 'vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+interface DataProps {
+    people: string[];
+    selectedPeron: string;
+    selectFun: (index: number) => void;
+}
+
+export default {
+    name: 'App',
+    setup() {
+        console.log('1-开始创建组件---setup()');
+        const data: DataProps = reactive({
+            people: ['张三', '李四', '王五'],
+            selectedPeron: "",
+            selectFun: (index: number) => {
+                data.selectedPeron = data.people[index]
+            }
+        })
+
+        onBeforeMount(() => {
+            console.log('2-组件挂载到页面之前执行---onBeforeMount()');
+        })
+
+        onMounted(() => {
+            console.log('3-组件挂载到页面之后执行---onMounted()');
+        })
+
+        onBeforeUpdate(() => {
+            console.log('4-组件更新之前执行---onBeforeUpdate()');
+        })
+
+        onUpdated(() => {
+            console.log('5-组件更新之后执行---onUpdated()');
+        })
+
+        // onRenderTracked((event) => {
+        //     console.log('状态跟踪钩子函数');
+        //     console.log(event)
+        // })
+
+        onRenderTriggered((event) => {
+            console.log('状态跟踪钩子函数');
+            console.log(event)
+        })
+
+        //onBeforeUnmount()组件卸载之前执行
+        //onUnmounted()组件卸载
+
+        //onActivated()  <keep-alive></keep-alive>
+        //onDeactivated()
+
+        //onErrorCaptured()  子组件异常时激活该函数
+
+        const refData = toRefs(data);
+
+        return {
+            ...refData
+        }
+    }
+};
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
+
